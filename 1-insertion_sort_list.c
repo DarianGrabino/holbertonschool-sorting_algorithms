0b6af85974a9a766ec733ad80aaa0623c04448b7
DarianGrabino <dariangra@gmail.com>
2023-05-19 06:28:48 -0700

    insertion_sort

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,49 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts using the Insertion sort algorithm
+ * @list: a double pointer to the head of the list
+ * Return: void
+*/
+
+void insertion_sort_list(listint_t **list)
+{
+	if (*list == NULL || (*list)->next == NULL)
+	{
+		return;
+	}
+
+	listint_t *current = (*list)->next;
+	while (current != NULL) 
+	{
+		listint_t *key = current;
+		listint_t *prev = current->prev;
+
+		current = current->next;
+
+		while (prev != NULL && key->n < prev->n) 
+		{
+			listint_t *next = key->next;
+			listint_t *prev_prev = prev->prev;
+
+			if (prev_prev != NULL)
+			{
+				prev_prev->next = key;
+			}
+			if (next != NULL) {
+				next->prev = prev;
+			}
+
+			prev->next = next;
+			prev->prev = key;
+			key->next = prev;
+			key->prev = prev_prev;
+
+			if (prev == *list) {
+				*list = key;
+            }
+			print_list(*list);
+			prev = prev_prev;
+        }
+    }
+}

a74b8dd46e8e712b3bdfa7a3ac91af844ce28d34
DarianGrabino <dariangra@gmail.com>
2023-05-19 08:06:26 -0700

    add prototype

--- sort.h
+++ sort.h
@@ -27,5 +27,6 @@ typedef struct listint_s
 void insertion_sort_list(listint_t **list);
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
 #endif

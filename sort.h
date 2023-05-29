f6274d3870213e5733ae0159c2973eabc350d432
DarianGrabino <dariangra@gmail.com>
2023-05-29 09:50:31 -0700

    add prototype

--- sort.h
+++ sort.h
@@ -28,5 +28,10 @@ void insertion_sort_list(listint_t **list);
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void swap(int *n1, int *n2);
+void bubble_sort(int *array, size_t size);
+void selection_sort(int *array, size_t size);
+
+
 
 #endif
